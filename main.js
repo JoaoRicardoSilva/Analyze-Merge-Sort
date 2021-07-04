634a521845c731c6697dcb432bb7a1f44f69ab1c
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-07-04 10:53:05 +0100

    Done: conquer() and divide()

--- main.js
+++ main.js
@@ -3,33 +3,36 @@
 const array = [
     1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
 ];
-
 const mergeSort = (arr) => {
-    const divideResult = []
-    const divide = (arg) => {
-        const length = arg.length;
-        if (length > 1) {
-            const middle = Math.ceil((length - 1) / 2);
-            const newArr1 = arg.slice(0, middle);
-            const newArr2 = arg.slice(middle);
-            console.log({ middle });
-            console.log({ newArr1 });
-            console.log({ newArr2 });
-            divide(newArr1);
-            divide(newArr2);
-        } else if (length === 1) {
-            console.log([arg]);
-            divideResult.push(arg)
-        } else {
-            console.log("Something is wrong");
+    const conquer = (l, r) => {
+        const result = [];
+
+        while (l.length && r.length) {
+            result.push(l[0] > r[0] ? r.shift() : l.shift());
         }
+
+        while (l.length) {
+            result.push(l.shift());
+        }
+        while (r.length) {
+            result.push(r.shift());
+        }
+        console.log(result);
+        return result;
+    };
+
+    const divide = (arg) => {
+        if (arg.length < 2) return arg;
+        console.log(arg);
+
+        const middle = Math.floor(arg.length / 2);
+        const left = arg.slice(0, middle);
+        const right = arg.slice(middle);
+
+        const sortedLeft = divide(left);
+        const sortedRight = divide(right);
+        return conquer(sortedLeft, sortedRight);
     };
     divide(arr);
-    console.log(divideResult);
-
-    const conquer = (arg) ={
-        
-    }
 };
-
 mergeSort(array);
